ce9437b82d3850f1cb990b273ff20ac01043bf3d
Julian Phillips <julian.p@me.com>
2019-11-11 12:01:25 -0500

    Completed FormField

--- bulma-lesson/src/App.js
+++ bulma-lesson/src/App.js
@@ -5,9 +5,20 @@ import 'bulma/css/bulma.css';
 
 // Custom Components
 import Navbar from './Components/Navbar/Navbar';
+import FormField from './Components/FormField/FormField';
 
 function App() {
-    return <Navbar />;
+    return (
+        <div>
+            <Navbar />
+            <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+            <FormField
+                label="Email"
+                type="email"
+                placeholder="e.g. alexsmith@gmail.com"
+            />
+        </div>
+    );
 }
 
 export default App;
